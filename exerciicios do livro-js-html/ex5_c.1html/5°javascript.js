9fe3b2e28dbd9d427068ff8b35eb44f0a89a6093
Nilson Rodrigues <nilsonrodrigues818@gmail.com>
2025-02-12 23:37:29 -0300

    exercícios

---
+++ exerciicios do livro-js-html/ex5_c.1html/5°javascript.js
@@ -0,0 +1,6 @@
+var fruta = "Banana"
+var preco = 3.50;
+var levar = true;
+var nonoValor;
+alert(fruta+preco+levar+ " "+ nonoValor);
+alert(fruta+ " "+ preco +" "+ levar+ " "+ nonoValor)
---
+++ exerciicios do livro-js-html/ex6_c.1html/6°javascript.js
@@ -0,0 +1,5 @@
+// lê um dado de entrada
+var num= prompt("Digite um número qualquer"); 
+var dobro= 2*num;
+// exibe a resposta
+alert("O dobro desse número  é "+dobro);
