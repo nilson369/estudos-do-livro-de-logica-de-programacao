24e17f662a5f64689c93dadcced8720294350c76
Nilson Rodrigues <nilsonrodrigues818@gmail.com>
2025-02-12 22:20:04 -0300

    javascript

---
+++ Livro-logica de programação e algoritmos com javascript/README.md
@@ -0,0 +1,2 @@
+# ESTUDO DO LIVRO: Lógica de Programação e Algoritmos com JavaScript
+Uma introdução á programação de computadores com exemplos e exe 
---
+++ Livro-logica de programação e algoritmos com javascript/ex3_1html/3°script.js
@@ -0,0 +1,11 @@
+var a ="20" 
+var b = 2*a;// b=40
+var c = a/2; // c=20
+var d = a-2; // d=18
+var e= a+2;  // e=22???
+alert("e:"+e); /* exibe o valor de uma variavel */
+<h2>Nas operações de multiplicação, divisão e subtração, a
+        linguagem converte esse texto em número e o valor retornado está de
+        acordo com o esperado. Contudo, quando realizamos a adição, o valor de
+        retorno é diferente do padrão, pois a linguagem concatena (+) o texto com
+        o número</h2>
